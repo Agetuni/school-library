07b56a433ad10d8b6cc356ad25a7cfb2ae42c248
Aleazar yilma <yilma4060@gmail.com>
2022-10-31 18:25:23 +0300

    add classes

---
+++ src/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def can_use_services?
+    return true if of_age? || @parent_permission == true
+
+    false
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
